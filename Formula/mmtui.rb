b61d54e4fd0aa5199de9750a6fa9974f9dffb44e
Nick Moore <nick.moore@pm.me>
2026-02-23 16:51:35 -0700

    Use stable v0.1.0 source in Homebrew formula

--- Formula/mmtui.rb
+++ Formula/mmtui.rb
@@ -1,11 +1,9 @@
 class Mmtui < Formula
   desc "Terminal user interface for NCAA March Madness brackets"
   homepage "https://github.com/holynakamoto/mmtui"
+  url "https://github.com/holynakamoto/mmtui/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "9a67c83133f86d036cc7e98c0f961aa3a490bc8cc72ae8964087a3259adcd9d7"
   license "MIT"
-
-  # Head formula so users can install directly from the tap without waiting
-  # for release artifact wiring.
-  head "https://github.com/holynakamoto/mmtui.git", branch: "main"
 
   depends_on "rust" => :build
 
@@ -14,6 +12,6 @@ class Mmtui < Formula
   end
 
   test do
-    assert_match "mmtui", shell_output("#{bin}/mmtui --version")
+    assert_match version.to_s, shell_output("#{bin}/mmtui --version")
   end
 end
