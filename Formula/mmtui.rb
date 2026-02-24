2dc554a3625f47509b59827a0ada03ffb5b2752d
Nick Moore <nick.moore@pm.me>
2026-02-24 07:11:19 -0700

    Update Homebrew formula to v0.1.8

--- Formula/mmtui.rb
+++ Formula/mmtui.rb
@@ -1,8 +1,8 @@
 class Mmtui < Formula
   desc "Terminal user interface for NCAA March Madness brackets"
   homepage "https://github.com/holynakamoto/mmtui"
-  url "https://github.com/holynakamoto/mmtui/archive/refs/tags/v0.1.7.tar.gz"
-  sha256 "1747bbbfcf8a3f7f745596e2679d5a0e384d03043adeb751c6faae6c6e2d8e8e"
+  url "https://github.com/holynakamoto/mmtui/archive/refs/tags/v0.1.8.tar.gz"
+  sha256 "0768796f96679e02e33900d17eccdefaafa7392b71f0a65254629673503c738e"
   license "MIT"
 
   depends_on "rust" => :build
